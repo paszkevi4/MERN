d16175548345478c9b13ca0138a5c0de208db4f6
paszkevi4 <paszkevi4@aol.com>
2020-07-13 13:47:03 +0300

    links.routes implemented

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require( 'jsonwebtoken' )
+const config = require( 'config' )
+
+module.exports = ( rew, res, next ) => {
+    if (req.method === 'OPTIONS' ) {
+        return next()
+    }
+
+    try {
+        const token = req.headers.authorization.split( ' ' )[1] //"Bearer TOKEN"
+
+        if (!token) {
+            res.status(401).json({ messages: 'no auth' })
+        }
+
+        const decoded = jwt.verify( token, config.get( 'jwtSecret' ))
+        req.user = decoded
+        next()
+
+    } catch (e) {
+        res.status(401).json({ messages: 'no auth' })
+    }
+}
